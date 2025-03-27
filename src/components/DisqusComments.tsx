eed7af85fe84d8bb5eee398167b7692fb050d3c6
johnycho <shunnn215@gmail.com>
2025-03-27 22:02:53 +0900

    댓글 버그 수정

--- src/components/DisqusComments.tsx
+++ src/components/DisqusComments.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import {DiscussionEmbed} from 'disqus-react';
 import {useLocation} from '@docusaurus/router';
 
 export default function DisqusComments() {
   const location = useLocation();
 
-  const disqusShortname = 'johny-dev'; // 너의 Disqus shortname
-  const disqusConfig = {
-    url: window.location.href,
-    identifier: location.pathname,
-    title: document.title,
-  };
-
   return (
-      <div style={{ marginTop: '2rem' }}>
-        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
-      </div>
+      <BrowserOnly fallback={<div>Loading comments...</div>}>
+        {() => {
+          const disqusShortname = 'johny-dev'; // 너의 shortname
+          const disqusConfig = {
+            url: window.location.href,
+            identifier: location.pathname,
+            title: document.title,
+          };
+
+          return (
+              <div style={{ marginTop: '2rem' }}>
+                <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+              </div>
+          );
+        }}
+      </BrowserOnly>
   );
 }
