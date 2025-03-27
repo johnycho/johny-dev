87a4e7de032230cd8784bdb051ac7f36d5cb7492
johnycho <shunnn215@gmail.com>
2025-03-27 16:20:11 +0900

    도큐먼트 삭제, 댓글 위젯 추가

--- docs/db/db-test.md
+++
@@ -1,7 +0,0 @@
----
-slug: db-test
-title: 크롤링 테스트
-tags: [ db ]
----
-
-크롤링 테스트야
---
+++ src/components/DisqusComments.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect} from 'react';
+
+export default function DisqusComments() {
+  useEffect(() => {
+    // Disqus가 이미 로드된 경우 재설정
+    if (window.DISQUS) {
+      window.DISQUS.reset({
+        reload: true,
+        config: function () {
+          this.page.url = window.location.href;
+          this.page.identifier = window.location.pathname;
+        },
+      });
+    } else {
+      // 최초 로드
+      const d = document;
+      const s = d.createElement('script');
+      s.src = 'https://johny-dev.disqus.com/embed.js'; // ← 여기에 본인의 shortname
+      s.setAttribute('data-timestamp', Date.now().toString());
+      s.async = true;
+      (d.head || d.body).appendChild(s);
+    }
+  }, []);
+
+  return (
+      <>
+        <div id="disqus_thread" style={{ marginTop: '2rem' }} />
+        <noscript>
+          Please enable JavaScript to view the{' '}
+          <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
+        </noscript>
+      </>
+  );
+}
